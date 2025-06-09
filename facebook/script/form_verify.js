14d993b41b243e6b5cd2c88a8b448919e3a236d4
claudiobentodala <programmer.claudio@gmail.com>
2025-06-09 15:47:49 +0100

    deleted comentary

--- facebook/script/form_verify.js
+++ facebook/script/form_verify.js
@@ -3,12 +3,7 @@ let elements = {
     password: window.document.querySelector('input#ipassword'),
     show: window.document.querySelector('p#warn')
 }
-/* LOCK PUT REGULAR EXPRESSION IN FORM VERIFY
-let regex_number_email = /([a-z][0-9]){3,}(\.[a-z][0-9]){2,}?\@([a-z][0-9]){2,}\.([a-z][0-9]){2,}(\.[a-z][0-9]){2,}? || ([0-9]){5,}/gi;
-let verify_number_email = regex_number_email.test(elements.email_number_email.value);
-let verify_regex;
 verify_number_email ? verify_regex = false : verify_regex = true ;
-*/
 function warn() {
     if (elements.email_number_email.value == "" && elements.password.value == "") {
         elements.show.innerText = "all textbox is empty" ;
