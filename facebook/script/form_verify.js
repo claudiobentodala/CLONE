9e54a90b59baa2da0cf0e0b67d672c781f5e75c3
claudiobentodala <programmer.claudio@gmail.com>
2025-06-09 11:45:57 +0100

    PUTTED COMENTARY

--- facebook/script/form_verify.js
+++ facebook/script/form_verify.js
@@ -1,13 +1,18 @@
 let elements = {
-    email_number: window.document.querySelector('input#iemail_numbe'),
+    email_number_email: window.document.querySelector('input#iemail_numbe'),
     password: window.document.querySelector('input#ipassword'),
     show: window.document.querySelector('p#warn')
 }
-
+/* LOCK PUT REGULAR EXPRESSION IN FORM VERIFY
+let regex_number_email = /([a-z][0-9]){3,}(\.[a-z][0-9]){2,}?\@([a-z][0-9]){2,}\.([a-z][0-9]){2,}(\.[a-z][0-9]){2,}? || ([0-9]){5,}/gi;
+let verify_number_email = regex_number_email.test(elements.email_number_email.value);
+let verify_regex;
+verify_number_email ? verify_regex = false : verify_regex = true ;
+*/
 function warn() {
-    if (elements.email_number.value == "" && elements.password.value == "") {
+    if (elements.email_number_email.value == "" && elements.password.value == "") {
         elements.show.innerText = "all textbox is empty" ;
-    } else if (elements.email_number.value == "") {
+    } else if (elements.email_number_email.value == "") {
         elements.show.innerText = "Check the email and phonenumber textbox" ;
     } else if (elements.password.value == "") {
         elements.show.innerText = "Check the password textbox" ;
